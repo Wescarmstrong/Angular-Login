bc160e369dd4471812fae7dc4a67f62c0211ba3e
Westley Armstotrong <you@example.com>
2020-02-06 19:51:00 -0600

    commit before push

--- src/app/components/log-in/log-in.component.ts
+++ src/app/components/log-in/log-in.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 
+interface LoginData {
+  email: string,
+  password: string  
+}
+
 @Component({
   selector: 'app-log-in',
   templateUrl: './log-in.component.html',
@@ -7,6 +12,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LogInComponent implements OnInit {
 
+loginData : LoginData = {
+  email: '',
+  password: ''
+};
   constructor() { }
 
   ngOnInit() {
